4c210b01300485dfed08d7a72c46625240865d4c
Etai Leers <Etaileers1993>
2021-04-09 16:00:29 +0300

    .

--- menu.py
+++ menu.py
@@ -1,14 +1,31 @@
 from tkinter import *
+import time
 
 
-class Menu:
+root = Tk()
 
-    def __init__(self):
-        root = Tk()
-        root.title('Input window V1')
-        root.geometry('1300x690')
-        root.resizable(False, False)
+root.title('Main Menu')
+root.geometry('1300x690')
+root.resizable(False, False)
 
-        root.mainloop()
+frame = Frame(root, width=1000, height=680)
+frame.configure(background="gray28")
+frame.pack(fill=BOTH, expand=True)
 
-        print('Hello GitHub')
+now = time.strftime("%H:%M:%S")
+clock_label = Label(root, bg="gray28", fg="white", pady=3, font=("Helvetica", 15))
+
+
+def display_time():
+    now = time.strftime("%H:%M:%S")
+    clock_label.configure(text=now)
+    root.after(20, display_time)
+
+
+display_time()
+
+clock_label.configure(text=now)
+clock_label.place(x=1135, y=40)
+clock_label.after(20, time)
+
+root.mainloop()
