01be225043264e454cab8bf1f70bda5dcddb62ad
Etai Leers <Etaileers1993>
2021-04-09 18:34:34 +0300

    Screes has been updated

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 # from login import Login, Register
 from inputScreen import Input
+from menu import MainMenu
 import datetime as dt
 import time
 import pandas as pd
@@ -41,13 +42,6 @@ class MainWindow:
         w = Label(self.app, text=f"{dt.datetime.now():%a, %b %d %Y}", bg="gray28", fg="white", pady=3, font=("Helvetica", 15))
         w.place(x=820, y=20)
 
-        #self.login = Button(self.app, text='Login',pady=5, padx=30, command=login)
-        self.login = Button(self.app, text='Login', pady=5, padx=30, command=input)
-        self.login.place(x=450, y=400)
-
-        self.register = Button(self.app, text='Register', pady=5, padx=20, command=register)
-        self.register.place(x=900, y=640)
-
         self.label = Label(self.app, bg="gray28", fg="white", pady=3, font=("Helvetica", 10), text='Forgot password?')
         self.label.place(x=445, y=440)
 
@@ -62,32 +56,48 @@ class MainWindow:
 
         self.usernameE = Entry(self.app, relief=FLAT, textvariable=self.usernameS)
         self.usernameE.place(x=490, y=305)
+
+        print(self.usernameE)
+
+        #self.login = Button(self.app, text='Login',pady=5, padx=30, command=login)
+        self.login = Button(self.app, text='Login', pady=5, padx=30, command=menu)
+        self.login.place(x=450, y=400)
+
+        #TODO: Undo hiding to the following line in order to enable users registration:
+
+        # self.register = Button(self.app, text='Register', pady=5, padx=20, command=register)
+        # self.register.place(x=900, y=640)
+
         self.passwordE = Entry(self.app, show='*', relief=FLAT, textvariable=self.passwordS)
         self.passwordE.place(x=490, y=355)
 
         bottom_header = Label(self.app, bg="gray28", fg="white", pady=3, font=("Helvetica", 15), text=('Hybrid Management - where Agile, TOC and waterfall meet together'))
         bottom_header.place(x=200, y=630)
 
-
     def run(self):
         self.app.mainloop()
-        #self.app.display_time()
+        self.app.display_time()
 
-'''
-def login():
-    loginTk = Login()
-    loginTk.run()
-'''
 
-def input():
+def menu():
     main.app.destroy()
-    inputTk = Input()
+    inputTk = MainMenu()
     inputTk.run()
 
-def register():
-    registerTk = Register()
-    registerTk.run()
+#Go straight to the Input screen:
 
+# def input():
+#     main.app.destroy()
+#     inputTk = Input()
+#     inputTk.run()
+
+# def register():
+#     registerTk = Register()
+#     registerTk.run()
+
+# def login():
+#     loginTk = Login()
+#     loginTk.run()
 
 main = MainWindow()
 main.run()
--- menu.py
+++ menu.py
@@ -1,31 +1,67 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import datetime as dt
 import time
+from tkinter import messagebox
+from inputScreen import Input
 
 
-root = Tk()
-
-root.title('Main Menu')
-root.geometry('1300x690')
-root.resizable(False, False)
-
-frame = Frame(root, width=1000, height=680)
-frame.configure(background="gray28")
-frame.pack(fill=BOTH, expand=True)
-
-now = time.strftime("%H:%M:%S")
-clock_label = Label(root, bg="gray28", fg="white", pady=3, font=("Helvetica", 15))
+def create_img(filename):
+    img = Image.open(filename)
+    img.thumbnail((100, 100))
+    p_img = ImageTk.PhotoImage(img)
+    return p_img
 
 
-def display_time():
-    now = time.strftime("%H:%M:%S")
-    clock_label.configure(text=now)
-    root.after(20, display_time)
+def message():
+    messagebox.showinfo("Hybrid Management - Notification", "We're sorry, this functionality is not supported yet.")
 
 
-display_time()
+class MainMenu:
 
-clock_label.configure(text=now)
-clock_label.place(x=1140, y=40)
-clock_label.after(20, time)
+    def __init__(self):
 
-root.mainloop()
+        root = Tk()
+        root.title('Main Menu')
+        root.geometry('1300x690')
+        root.resizable(False, False)
+
+        frame = Frame(root, width=1000, height=680)
+        frame.configure(background="gray28")
+        frame.pack(fill=BOTH, expand=True)
+
+        w = Label(root, text=f"{dt.datetime.now():%a, %b %d %Y}", bg="gray28", fg="white", pady=3, font=("Helvetica", 15))
+        w.place(x=1100, y=15)
+
+        now = time.strftime("%H:%M:%S")
+        clock_label = Label(root, bg="gray28", fg="white", pady=3, font=("Helvetica", 15))
+
+        image2 = create_img('Capture.JPG')
+        img2 = Label(frame, image=image2)
+        img2.image = image2
+        img2.place(x=15, y=15)
+
+        bottom_header = Label(root, bg="gray28", fg="white", pady=3, font=("Helvetica", 30), text='Main Menu:')
+        bottom_header.place(x=540, y=155)
+
+        button1 = Button(root, text="Insert New Project", command=input)
+        button1.config(bg="aquamarine2", pady=10, padx=20, width=20, height=4)
+        button1.place(x=350, y=320)
+
+        button2 = Button(root, text="Projects Records", command=message)
+        button2.config(bg="aquamarine2", pady=10, padx=20, width=20, height=4)
+        button2.place(x=550, y=320)
+
+        button3 = Button(root, text="Settings", command=message)
+        button3.config(bg="aquamarine2", pady=10, padx=20, width=20, height=4)
+        button3.place(x=750, y=320)
+
+        bottom_header = Label(root, bg="gray28", fg="white", pady=3, font=("Helvetica", 15),
+                              text='Hybrid Management - where Agile, TOC and waterfall meet together')
+        bottom_header.place(x=360, y=650)
+
+        # root.mainloop()
+
+def input():
+    inputTk = Input()
+    inputTk.run()
